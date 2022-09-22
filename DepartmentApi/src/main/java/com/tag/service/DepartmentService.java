8959431b907d8d896feb92247752d0cda74afcd1
Pradeep Nishad <106637122+Pradeepnishad354@users.noreply.github.com>
2022-09-22 10:28:02 +0530

    v1

---
+++ DepartmentApi/src/main/java/com/tag/service/DepartmentService.java
@@ -0,0 +1,23 @@
+package com.tag.service;
+
+import com.tag.entity.Department;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
+
+public interface DepartmentService {
+
+
+     Department SaveDepartment(Department department);
+
+     List<Department> fetchDepartmentList();
+
+
+     void deleteDepartmentById(Long departmentId);
+
+      Department updateDepartment( Department department, Long departmentId);
+
+
+
+}
